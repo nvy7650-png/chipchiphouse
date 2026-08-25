bfacce3f52c2792bc9b0bb438c198f821a868342
Vy <nvy7650@gmail.com>
2026-08-25 15:50:10 +0700

    Fix routes path

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const cors = require('cors');
 
 const app = express();
 
-// Middleware giải mã JSON từ Request Body & cho phép Frontend gọi API (CORS)
 app.use(cors());
 app.use(express.json());
 
-// Khai báo Route Auth
 const authRoutes = require('./routes/auth.routes');
 app.use('/api/auth', authRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server đang chạy tại port ${PORT}`);
 });
